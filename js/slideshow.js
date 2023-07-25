586caf37fe7008da79c9ae708abe79b1828e2218
Karim Khasan <karimhasan3879@gmail.com>
2023-07-25 03:00:37 +0300

    feat: add slides autoswitch

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,37 +1,76 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+const refreshTime = 5000;
+let timer;
+let galleryContainer;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+window.addEventListener("load", function () {
+  showSlides(slideIndex);
+  timer = setInterval(function () {
+    plusSlides(1)
+  }, refreshTime);
+
+  galleryContainer = document.getElementsByClassName('gallery-container')[0];
+  galleryContainer.addEventListener('mouseenter', pause)
+  galleryContainer.addEventListener('mouseleave', resume)
+})
+
+  function plusSlides(n) {
+    clearInterval(timer);
+    if (n < 0) {
+      showSlides(slideIndex -= 1);
+    } else {
+      showSlides(slideIndex += 1);
+    }
+
+    if (n === -1) {
+      timer = setInterval(function () {
+        plusSlides(n + 2)
+      }, refreshTime);
+    } else {
+      timer = setInterval(function () {
+        plusSlides(n + 1)
+      }, refreshTime);
+    }
+  }
+
+  function currentSlide(n) {
+    clearInterval(timer);
+    timer = setInterval(function(){plusSlides(n + 1)}, refreshTime);
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const captions = document.getElementsByClassName("slide-caption")
+    const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n <= 0) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; ++i) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < slides.length; ++i) {
+      captions[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; ++i) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    captions[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
+
+pause = () => {
+  clearInterval(timer);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("slide");
-  const captions = document.getElementsByClassName("slide-caption")
-  const dots = document.getElementsByClassName("dot");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n <= 0) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; ++i) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < slides.length; ++i) {
-    captions[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; ++i) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  captions[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+resume = () =>{
+  clearInterval(timer);
+  timer = setInterval(function(){plusSlides(slideIndex)}, refreshTime);
 }
