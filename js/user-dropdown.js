bedfbb09c13182119424896dc1e76c8c6fea7d02
Karim Khasan <karimhasan3879@gmail.com>
2023-07-26 22:11:54 +0300

    feat: add user dropdown

---
+++ js/user-dropdown.js
@@ -0,0 +1,23 @@
+import { isLoggedIn } from './auth.js'
+
+const userBtn = document.getElementById('user-btn');
+const userDropdown = document.getElementById('user-dropdown');
+const userLinks = userDropdown.querySelectorAll('a');
+
+userBtn.addEventListener('click', () => {
+  userDropdown.style.display = (userDropdown.style.display === 'flex') ? 'none' : 'flex';
+});
+
+window.addEventListener('click', (event) => {
+  if (!userDropdown.contains(event.target) && !userBtn.contains(event.target)) {
+    userDropdown.style.display = 'none';
+  }
+});
+
+userLinks.forEach((link) => {
+  if (isLoggedIn()) {
+    link.href = '#';
+  } else {
+    link.href = 'login.html';
+  }
+});
